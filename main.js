0e758ee8737db74ff72ba4e9862ff9f90690cf6d
ordiaprecious <ordiaprecious@gmail.com>
2023-10-09 18:49:58 +0100

    last correction2

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-// prompt('put your account number here')
-// alert('YOU SEF RUN PUT AM LONG THROAT')
+prompt('put your account number here')
+alert('YOU SEF RUN PUT AM LONG THROAT')
 var texts=['HAPPY BIRTHDAY', 'KING ERI', 'WE WISH YOU', 'LONG LIFE AND PROSPERITY', ' MAY YOU CONTINUE TO PROSPER' ,'IN EVERYTHING YOU DO','IN JESUS NAME', 'AMEN!!', 'WE LOVE YOU', 'BECKINS DONT EAT HIS BISCUITS O' ,'OYA STOP CLICKING', 'I SAID STOP CLICKING'];
 var tex=0
 function press(){
