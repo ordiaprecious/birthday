0abf125f386725b45e85b089129949369285f3bf
ordiaprecious <ordiaprecious@gmail.com>
2023-10-09 14:48:45 +0100

    first bd

---
+++ main.js
@@ -0,0 +1,8 @@
+var texts=['HAPPY BIRTHDAY KING ERI', ' I WISH YOU LONG LIFE AND PROSPERITY', ' MAY YOU CONTINUE TO PROSPER IN EVERYTHING YOU DO', 'WE LOVE YOU', 'BECKINS DONT EAT HIS BISCUITS O' ,' OYA STOP CLICKING', ' I SAID STOP CLICKING'];
+var tex=0
+function press(){
+    document.getElementById('contd').innerText=texts[tex++] ;
+   
+    console.log('fhbfh')
+
+}
