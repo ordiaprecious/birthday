add657ad5c37960ef8087bd8f77724bf026d1283
ordiaprecious <ordiaprecious@gmail.com>
2023-10-09 18:48:03 +0100

    last correction

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
-var texts=['HAPPY BIRTHDAY KING ERI', ' I WISH YOU LONG LIFE AND PROSPERITY', ' MAY YOU CONTINUE TO PROSPER IN EVERYTHING YOU DO', 'WE LOVE YOU', 'BECKINS DONT EAT HIS BISCUITS O' ,' OYA STOP CLICKING', ' I SAID STOP CLICKING'];
+// prompt('put your account number here')
+// alert('YOU SEF RUN PUT AM LONG THROAT')
+var texts=['HAPPY BIRTHDAY', 'KING ERI', 'WE WISH YOU', 'LONG LIFE AND PROSPERITY', ' MAY YOU CONTINUE TO PROSPER' ,'IN EVERYTHING YOU DO','IN JESUS NAME', 'AMEN!!', 'WE LOVE YOU', 'BECKINS DONT EAT HIS BISCUITS O' ,'OYA STOP CLICKING', 'I SAID STOP CLICKING'];
 var tex=0
 function press(){
-    document.getElementById('contd').innerText=texts[tex++] ;
+    document.getElementById('contd').innerText=texts[tex++];
    
     console.log('fhbfh')
 
