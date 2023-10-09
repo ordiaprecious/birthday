f70b3149f48724212458d800265d9a5eaa9acb4a
ordiaprecious <ordiaprecious@gmail.com>
2023-10-09 18:55:27 +0100

    last correction3

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
-prompt('put your account number here')
+prompt('Put your Account number here')
 alert('YOU SEF RUN PUT AM LONG THROAT')
 var texts=['HAPPY BIRTHDAY', 'KING ERI', 'WE WISH YOU', 'LONG LIFE AND PROSPERITY', ' MAY YOU CONTINUE TO PROSPER' ,'IN EVERYTHING YOU DO','IN JESUS NAME', 'AMEN!!', 'WE LOVE YOU', 'BECKINS DONT EAT HIS BISCUITS O' ,'OYA STOP CLICKING', 'I SAID STOP CLICKING'];
 var tex=0
 function press(){
     document.getElementById('contd').innerText=texts[tex++];
-   
-    console.log('fhbfh')
 
 }
